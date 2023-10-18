32fd6f57c5f7d2664facbd4425e2f0480f5cb737
akimabs <>
2023-10-18 16:05:56 +0700

    update endpoint test cpu

--- src/main/java/com/example/example/ExampleController.java
+++ src/main/java/com/example/example/ExampleController.java
@@ -1,6 +1,7 @@
 package com.example.example;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,5 +15,34 @@ public class ExampleController {
     @RequestMapping("/health")
     public String health() {
         return "Health check";
+    }
+
+    @RequestMapping("/fibonanci")
+    public String fibonacciEndpoint() {
+        return fibonacci(42);
+    }
+
+    public String fibonacci(int n) {
+        if (n < 0) {
+            return "Invalid input. Please provide a non-negative number.";
+        }
+
+        // Generate the Fibonacci sequence iteratively
+        StringBuilder result = new StringBuilder();
+        int first = 0, second = 1;
+        result.append("Fibonacci Sequence for n = " + n + ":\n");
+
+        if (n >= 1) {
+            result.append(first);
+        }
+
+        for (int i = 1; i < n; i++) {
+            result.append(", ").append(second);
+            int next = first + second;
+            first = second;
+            second = next;
+        }
+
+        return result.toString();
     }
 }
